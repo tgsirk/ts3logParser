dcf310a8f0f5575fbf3ce7944f3940e22cebf203
tgsirk <kris.zxc@gmail.com>
2021-12-25 13:59:52 +0100

    fix mysql empty inserts

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def parsing_cycle():
     mysql_staff.create_table()
     start_query_date = mysql_staff.get_last_existing_timestamp()
     registered_users_list = searching_engine.search_logs(start_query_date)
-    mysql_staff.insert_values(registered_users_list)
+    if len(registered_users_list) < 1:
+        mysql_staff.insert_values(registered_users_list)
 
 
 while True:
