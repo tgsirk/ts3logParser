bf1d26c9b4fb668314fb5415daacf45192e666db
tgsirk <kris.zxc@gmail.com>
2022-01-13 17:31:02 +0100

    fix group id in configuration.py

--- configuration.py
+++ configuration.py
@@ -1,7 +1,7 @@
 import datetime
 
 REGISTER_USER_STATEMENT_0 = "was added to servergroup 'Zweryfikowany | TS3'(id:7) by client"
-REGISTER_USER_STATEMENT_1 = "was added to servergroup 'Zweryfikowana | TS3'(id:7) by client"
+REGISTER_USER_STATEMENT_1 = "was added to servergroup 'Zweryfikowana | TS3'(id:193) by client"
 LOG_SECTION_LIMITER = "|"
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 MYSQL_HOST = '192.168.0.29'
